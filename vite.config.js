c1bee2eefc09ea94a6bccbabfcfbd7d514a48e35
Roberto Carlos Maldonado Morán <a20171016@pucp.edu.pe>
2025-08-16 22:30:05 -0500

    vite

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+export default defineConfig({
+    plugins: [basicSsl()],
+    server: { https: true, host: true } // expone en LAN y con https
+})
